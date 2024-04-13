e99645e2f03dde9b31684eafacdd656e24f8183f
Colten Haggard <124627990+Colten-Haggard-dev@users.noreply.github.com>
2024-04-13 14:47:28 -0500

    Added topnav
    Contains basic header info that we can tweak to our hearts' content later.

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,102 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import AccountIcon from "./lib/AccountIcon.svelte"
+  
+  const active = "active"
+  const inactive = "inactive"
+
+  $: bclasses = [inactive, inactive, inactive, inactive]
+
+  let account_icon: AccountIcon
+
+  function update_classes(idx: number): void {
+    for (let index = 0; index < bclasses.length; index++) {
+      bclasses[index] = inactive
+    }
+
+    bclasses[idx] = active
+  }
+
+  function update_nav(str: string): void {
+    switch (str) {
+      case "home":
+        update_classes(0)
+        break;
+      case "news":
+        update_classes(1)
+        break;
+      case "contact":
+        update_classes(2)
+        break;
+      case "about":
+        update_classes(3)
+        break;
+    
+      default:
+        break;
+    }
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="topnav">
+    <a class={bclasses[0]} on:click={() => update_nav("home")} href="#home">Home</a>
+    <a class={bclasses[1]} on:click={() => update_nav("news")} href="#news">News</a>
+    <a class={bclasses[2]} on:click={() => update_nav("contact")} href="#contact">Contact</a>
+    <a class={bclasses[3]} on:click={() => update_nav("about")} href="#about">About</a>
+    <a style="float: right;" on:click={() => account_icon.logIn("Rowdy")} on:mouseenter={() => account_icon.useHover(true)} on:mouseleave={() => account_icon.useHover(false)} href="#signup"> <AccountIcon bind:this={account_icon} /> </a>
   </div>
-  <h1>Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
   </div>
 
-  <p>
+  <p class="checkout">
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
   </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .topnav {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  /* Add a black background color to the top navigation */
+  .topnav {
+    background-color: #0C2340;
+    overflow: hidden;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  /* Style the links inside the navigation bar */
+  .topnav a {
+    float: left;
+    color: #F15A22;
+    text-align: center;
+    padding: 16px 16px;
+    text-decoration: none;
+    font-size: 20px;
   }
-  .read-the-docs {
-    color: #888;
+
+  /* Change the color of links on hover */
+  .topnav a:hover {
+    background-color: #F15A22;
+    color: #0C2340;
+  }
+
+  .checkout {
+    color: #F15A22;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+
+  /* Add a color to the active/current link */
+  .topnav a.active {
+    background-color: #F15A22;
+    color: white;
   }
 </style>
---
+++ src/lib/AccountIcon.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  const active = "active"
+  const inactive = "inactive"
+
+  $: current = inactive
+  $: loggedin = false
+  $: username = ""
+
+  export function logIn(uname: string) {
+    loggedin = true
+    username = uname
+  }
+
+  export function useHover(isactive: boolean): void
+  {
+    if (isactive)
+      current = active
+    else
+      current = inactive
+  }
+</script>
+
+<div class={current}>
+  {#if loggedin}
+    <img src="/UTSA-Roadrunners-Logo.png" alt="Account icon" width="32" height="32"/>
+    {username}
+  {:else}
+    Login/SignUp
+  {/if}
+  
+</div>
+
+<style>
+  * {
+    text-align: center;
+    float: left;
+  }
+
+  /* Change the color of links on hover */
+  .active {
+    color: #0C2340;
+  }
+
+  /* Style the links inside the navigation bar */
+  .inactive {
+    color: #F15A22;
+  }
+</style>
