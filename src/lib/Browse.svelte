8a528b1b6aa004004b4eaaa0a26f2792114168f3
Colten Haggard <124627990+Colten-Haggard-dev@users.noreply.github.com>
2024-04-17 12:01:13 -0500

    Big update

---
+++ src/lib/Browse.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+</script>
+
+<main>
+  <div class="filter">
+    <label for="search">Search:</label>
+    <input style="min-width: 20vw;" type="text" id="search" name="search" placeholder="Try searching for new clothes..."><br><br>
+  </div>
+  <div class="listings">
+    <p>oh boy</p>
+  </div>
+</main>
+
+<style>
+  .filter {
+    min-width: 66vw;
+    min-height: 10vh;
+  }
+
+  .listings {
+    min-width: 66vw;
+    min-height: 66vh;
+    background-color: #DBDEE3;
+    outline: 2px solid blue;
+  }
+</style>
---
+++ src/lib/LandingPage.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+</script>
+
+<main>
+  <h1>
+    Welcome to Rowdy Marketplace!
+  </h1>
+  <p>
+    This is a user driven marketplace for UTSA students to sell things to other people on Campus.
+    You can sign up for an account or browse before you buy.
+  </p>
+  <button>
+    Browse
+  </button>
+</main>
+
+<style>
+  h1 {
+    padding-bottom: 1em;
+  }
+</style>
+  
