c3e5bfb13553fe99d0509b8d39630162750136a7
SebastianF <109153703+SebF23@users.noreply.github.com>
2024-04-23 18:54:58 -0500

    Update adminOrders.svelte

--- src/pages/adminOrders.svelte
+++ src/pages/adminOrders.svelte
@@ -1,6 +1,47 @@
-<script>
-  import AdminSideNav from "../lib/AdminSideNav.svelte";
+<script lang="ts">
   import Topnav from "../lib/Topnav.svelte";
+  import AdminSideNav from "../lib/AdminSideNav.svelte";
+  import { onMount } from "svelte";
+  let aOrders: Order[] = [];
+  let dir: string = "asc"
+  let sort: string = "unsorted"
+
+  async function fetchOrders(): Promise<Order[] | undefined> {
+    try {
+      const url_end = sort != "unsorted" ? "Orders?sort=" + sort + "&direction=" + dir : "Orders";
+      const url = "http://localhost:8080/api/Oders?sort=" + dir
+      console.log(url)
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Accept": "application/json"
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const orders: Order[] = await response.json();
+      console.log("Orders fetched successfully:", orders);
+
+      // Here you can manipulate the fetched data
+      return orders;
+    } catch (error) {
+      console.error("Error fetching items:", error);
+      return undefined;
+    }
+  }
+
+
+  onMount(async () => {
+		// Example of how to use the fetchItems function
+    await fetchOrders().then(orders => {
+      if (orders) {
+      aOrders = orders
+      }
+    });
+	}
 </script>
 
 <main>
