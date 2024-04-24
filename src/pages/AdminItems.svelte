94ef5963825a49497b7e25872aedf5618a326f8d
SebastianF <109153703+SebF23@users.noreply.github.com>
2024-04-23 21:36:30 -0500

    minor update

--- src/pages/AdminItems.svelte
+++ src/pages/AdminItems.svelte
@@ -1,11 +1,61 @@
-<script>
-import Topnav from "../lib/Topnav.svelte";
-import AdminSideNav from "../lib/AdminSideNav.svelte";
+<script lang="ts">
+  import Topnav from "../lib/Topnav.svelte";
+  import AdminSideNav from "../lib/AdminSideNav.svelte";
+  import { onMount } from "svelte";
+  let aItems: Item[] = [];
+  let dir: string = "asc"
+
+  async function fetchItems(): Promise<Item[] | undefined> {
+    try {
+      const url = "http://localhost:8080/api/Item?sort=" + dir
+      console.log(url)
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Accept": "application/json"
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const items: Item[] = await response.json();
+      console.log("Orders fetched successfully:", items);
+
+      // Here you can manipulate the fetched data
+      return items;
+    } catch (error) {
+      console.error("Error fetching items:", error);
+      return undefined;
+    }
+  }
+
+  async function onChange() {
+    await fetchItems().then(items => {
+      if (items) {
+        aItems = items
+      }
+    });
+  }
+
+  onMount(async () => {
+		// Example of how to use the fetchItems function
+    await fetchItems().then(items => {
+      if (items) {
+        aItems = items
+      }
+    });
+	});
+
 </script>
 
 <main>
   <Topnav />
-  <div class="sideNav"><AdminSideNav /></div>
+  <div class="sideNav">
+    <AdminSideNav />
+  </div>
+  <div>This is the user view and modify page for admins</div>
 </main>
 
 <style>
@@ -20,3 +70,4 @@ import AdminSideNav from "../lib/AdminSideNav.svelte";
     min-height: 100%
   }
 </style>
+  
